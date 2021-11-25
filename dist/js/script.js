e40ab3c178ff899aa98da8b39ee7938b5e7aa8be
lena <ptashnuik@gmail.com>
2021-11-25 15:36:42 +0200

    finish

--- dist/js/script.js
+++ dist/js/script.js
@@ -40,3 +40,19 @@ for (i = 0; i < slides.length; i++) {
 
 slides[slideIndex-1].style.display = "block";
 }
+
+var acc = document.getElementsByClassName("faq__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
